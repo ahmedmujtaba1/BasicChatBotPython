274457b0e2991310620f4fdac0d7f4cf059a7d65
ahmedmujtaba1 <ahmed.mujtaba11737@hhs.edu.pk>
2023-10-27 18:10:23 +0500

    Update main.py

--- main.py
+++ main.py
@@ -21,4 +21,6 @@ words = [lemmatizer.lemmatize(word) for word in words if word not in ignoreLette
 words = sorted(set(classes))
 classes = sorted(set(classes))
 
-pickle.dump(words, )
+pickle.dump(words, open('words.pkl', 'wb'))
+pickle.dump(classes, open('classes.pkl', 'wb'))
+
