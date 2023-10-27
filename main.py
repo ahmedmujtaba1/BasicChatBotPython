aedbe4a9cbe55f129174a1dddbdcc19a0db0f6ab
ahmedmujtaba1 <ahmed.mujtaba11737@hhs.edu.pk>
2023-10-27 16:39:12 +0500

    Writing Main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import random, json, pickle, numpy as np, tensorflow as tf, nltk
+from nltk.stem import WordNetLemmatizer
+
+lemmatizer = WordNetLemmatizer()
+intents = json.loads(open('intents.json').read())
+
+words = []
+words = []
+words = []
